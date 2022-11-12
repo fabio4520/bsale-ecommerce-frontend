5c6b43516a31107e6c3174582ffee709798826c9
fabio4520 <fabioleofc@gmail.com>
2022-11-12 10:04:01 -0500

    ADD: navbar Problem - Needs to add navbar Solution - Add tailwind for styles - Add navbar component

--- scripts/pages/homepage.js
+++ scripts/pages/homepage.js
@@ -1,16 +1,48 @@
+import { NavBar } from "../components/navbar.js";
+import DOMHandler from "../dom-handler.js"
+
 function render() {
   return `
-    <h1 class="text-3xl font-bold">
-      Hello world!
-    </h1>
+  <div class='flex justify-center items-center'>
+    ${NavBar()}
+  </div>
   `;
 }
 
+function listenHomeBtn() {
+  const homeAnchor = document.querySelector("#home-btn")
+  homeAnchor.addEventListener("click", (event) => {
+    event.preventDefault();
+    DOMHandler.reload();
+  })
+}
+function listenProductsBtn() {
+  const productsAnchor = document.querySelector("#products-btn")
+  productsAnchor.addEventListener("click", (e)=> {
+    e.preventDefault();
+  })
+}
+function listenCategoriesBtn() {
+  const categoriesAnchor = document.querySelector("#categories-btn")
+  categoriesAnchor.addEventListener("click", (e) => {
+    e.preventDefault();
+  })
+}
+function listenLogo() {
+  const logoBtn = document.querySelector("#logo-btn");
+  logoBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    DOMHandler.load(HomePage);
+  })
+}
 export const HomePage = {
   toString() {
     return render();
   },
   addListeners() {
-    
+    listenHomeBtn(),
+    listenProductsBtn(),
+      listenCategoriesBtn(),
+      listenLogo()
   }
 }
