fa6b52b92816baa417060c7894a5f26bd68f40ed
fabio4520 <fabioleofc@gmail.com>
2022-11-12 10:28:12 -0500

    ADD: fake data into page

---
+++ data/categories.js
@@ -0,0 +1,20 @@
+const categories = [
+  {
+    id: 1,
+    name: 'category 1'
+  },
+  {
+    id: 2,
+    name: 'category 2'
+  },
+  {
+    id: 3,
+    name: 'category 3'
+  },
+  {
+    id: 4,
+    name: 'category 4'
+  },
+]
+
+export default categories
